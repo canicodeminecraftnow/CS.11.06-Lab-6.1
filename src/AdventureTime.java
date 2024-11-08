7740cff9e44089d1f32388019d7d806983fa723d
canicodeminecraftnow <hoktsungjamesl2027@student.cis.edu.hk>
2024-11-08 16:30:51 +0800

    Update AdventureTime.java

--- src/AdventureTime.java
+++ src/AdventureTime.java
@@ -22,31 +22,63 @@ public class AdventureTime {
      * @throws IOException
      */
     public static int challengeOne(String fileName) throws IOException {
-        return 0;
-    }
+        int donald = 0;
+        int[] trump = readFile(fileName);
+        for (int i = 0; i < trump.length; i++) {
+            if (trump[i] < trump[i - 1]) {
+                donald = donald + 1;
+            }
+            return donald;
+        }
 
-    /** TODO 2
-     *
-     * Challenge 2
-     *
-     * @param fileName
-     * @return Answer to Challenge 2
-     * @throws FileNotFoundException
-     */
-    public static int challengeTwo(String fileName) throws FileNotFoundException {
-        return 0;
-    }
 
-    /** TODO 3
-     *
-     * Challenge 3
-     *
-     * @param fileName
-     * @return Answer to Challenge 3
-     * @throws FileNotFoundException
-     */
-    public static int challengeThree(String fileName) throws FileNotFoundException {
-        return 0;
+        /** TODO 2
+         *
+         * Challenge 2
+         *
+         * @param fileName
+         * @return Answer to Challenge 2
+         * @throws FileNotFoundException
+         */
+        public static int challengeTwo (String fileName) throws FileNotFoundException {
+            int increase = 0;
+            int[] file = readFile(fileName);
+            for (int i = 1; i < file.length - 2; i++) {
+                if (file[i] + file[i + 2] + file[i + 3] > file[i - 1] + file[i] + file[i + 1]) {
+                    increase = increase + 1;
+                }
+            }
+            return increase;
+        }
+
+        /** TODO 3
+         *
+         * Challenge 3
+         *
+         * @param fileName
+         * @return Answer to Challenge 3
+         * @throws FileNotFoundException
+         */
+        public static int challengeThree (String fileName) throws FileNotFoundException int motion;
+        {
+            int vertical = 0;
+            int horizontal = 0;
+            motion = vertical * horizontal;
+            int[] file = readFile(fileName);
+            for (int i = 0; i < file.length; i++) {
+                String word = "horizontal";
+                String word2 = "vertical";
+                if (file[i].indexOf(word) != -1) {
+                    vertical = vertical + file[i];
+                } else if (file[i].indexOf(word2) != -1) {
+                    horizontal = horizontal + file[i];
+
+                }
+
+            }
+
+        }
+        return motion;
     }
 
     /** TODO 4
@@ -58,7 +90,50 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeFour(String filename) throws FileNotFoundException {
-        return 0;
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+
+        // Read the file and get its lines as an array of Strings
+        String[] commands = readFile(filename);
+
+        for (int i = 0; i < commands.length; i++) {
+            String commandLine = commands[i].trim().toLowerCase();
+
+
+            String[] parts = commandLine.split("\\s+");
+
+
+            String command = parts[0];
+            int value;
+
+            try {
+                value = Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid number format in line: " + commands[i]);
+                continue;
+            }
+
+            switch (command) {
+                case "down":
+                    aim += value;
+                    break;
+                case "up":
+                    aim -= value;
+                    break;
+                case "forward":
+                    horizontal += value;
+                    depth += aim * value;
+                    break;
+                default:
+                    System.err.println("Unknown command in line: " + commands[i]);
+
+                    break;
+            }
+        }
+
+        // Return the product of horizontal position and depth
+        return horizontal * depth;
     }
 
     /** This method will write the values passed as challengeOne, challengeTwo, challengeThree, and challengeFour to a text file.
